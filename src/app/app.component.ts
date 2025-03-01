9b33944cb950efe89a86877227e92b673b0f1110
gerome <geromesinoy14@gmail.com>
2025-03-01 19:01:57 +0800

    Fetch Project from Server 1:17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/types.ts
@@ -0,0 +1,14 @@
+import { HttpContext, HttpHeaders, HttpParams } from "@angular/common/http";
+
+export interface Options {
+          headers?: HttpHeaders | Record<string, string | string[]>;
+          observe: 'body';
+          context?: HttpContext;
+          params?: HttpParams | Record<string, string | number | boolean | ReadonlyArray<string | number | boolean>>;
+          reportProgress?: boolean;
+          responseType?: 'json';
+          withCredentials?: boolean;
+          transferCache?: {
+              includeHeaders?: string[];
+          } | boolean;
+      }
